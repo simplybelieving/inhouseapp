99dad55e1f8f48e24442024719ac4241fe75d759
simplybelieving <38836900+simplybelieving@users.noreply.github.com>
2019-03-28 21:40:23 +0800

    Update app.js

--- public/app/app.js
+++ public/app/app.js
@@ -1,5 +1,5 @@
 //main config for all angular files
 
-angular.module('productApp', ['appRoutes',  'authControllers', 'authServices', 'userServices', 'userControllers', 'pricelistServices', 'pricelistControllers','productServices', 'productControllers', 'clientServices', 'clientControllers']).run(function($rootScope, $location){
+angular.module('productApp', ['appRoutes',  'poControllers',  'authControllers', 'authServices', 'userServices', 'userControllers', 'pricelistServices', 'pricelistControllers','productServices', 'productControllers', 'clientServices', 'clientControllers']).run(function($rootScope, $location){
    $rootScope.location = $location;
 });
