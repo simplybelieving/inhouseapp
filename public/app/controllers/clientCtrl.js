49854964a0ee9fe27b396cfef871d06d951ee026
simplybelieving <38836900+simplybelieving@users.noreply.github.com>
2019-04-07 17:49:12 +0800

    Update clientCtrl.js

--- public/app/controllers/clientCtrl.js
+++ public/app/controllers/clientCtrl.js
@@ -1,4 +1,4 @@
-angular.module('clientControllers', ['productServices','clientServices', 'ui.bootstrap', 'angular-loading-bar'])
+angular.module('clientControllers', ['clientServices', 'ui.bootstrap', 'angular-loading-bar'])
 
 .controller('invoiceCtrl', function($http, $scope, Product, Client){
 var app = this;
@@ -8,13 +8,6 @@ app.items = data.data.items;
 console.log(app.items);
 });
 Client.getClients();
-
-
-Product.getProducts().then(function(data) {
-app.products = data.data.items;
-console.log(app.products[1].productname);
-});
-//Product.getProducts();
 
 
 $scope.qty = [];
@@ -45,8 +38,9 @@ $scope.type = data.data.client.type;
 app.showdetails = function(newaddress, newname, newdate, pricelistArray){  };
 
 this.editPrice = function(type, pricelistArray, $index){
-console.log("client is"+ type);
-$http.post('/api/editPricelist', {client_type:type, productname:this.pricelistArray[$index]._productname, newPrice:this.pricelistArray[$index].price, product_id:this.pricelistArray[$index]._id} ).then(function(data){
+
+
+$http.post('/api/editPricelist', {discount:this.pricelistArray[$index].discount, client_type:type, productname:this.pricelistArray[$index]._productname, newPrice:this.pricelistArray[$index].price, product_id:this.pricelistArray[$index]._id} ).then(function(data){
 
    if(data.data.success){ app.successMsg= data.data.message; } else{ app.errorMsg = data.data.message;}
 
